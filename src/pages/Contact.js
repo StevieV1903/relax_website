2c81911da4abaa1a128d019e82e7bd06c23a0c28
Steve Vance <steve.vance@hotmail.co.uk>
2021-09-01 14:32:56 +0100

    fixed container padding issue and added small padding to dropdown icon

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -9,8 +9,10 @@ const Contact = () => {
         <>
         <div className="contact-page-container">
             <div className="sapphire-content">
+                <div className="contact-intro">
                 <h2 className="contact-title">Contact Us...</h2>
                 <p>If you would like to have a chat about any of the services that we deliver, or have a chat about anything from available resources to management techniques or coping strategies, please feel free to contact us using the details below or through any of our social media channels which are accessesible through the links at the bottom of this website. We look forward to hearing from you. </p>
+                </div>
                     <div className="contact-info-container">
                         <div className="contact-item">
                         <i className='fas fa-phone'/>
