a417de403fb266b1dda4cc6b663f5ccd7f4f4960
Steve Vance <steve.vance@hotmail.co.uk>
2022-02-07 12:29:35 +0000

    gallery images and captions working not responsive yet

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../App.css';
 import './Gallery.css';
 // import ImageSlider from '../components/ImageSlider.js'
@@ -9,48 +9,74 @@ import image4 from '../assets/service2Hero.png';
 
 const Gallery = () => {
 
-    const ImageSliderData = [
+    const [ currentSlide, setCurrentSlide ] = useState(0);
+
+    const imageSliderData = [
         {
             image: image1,
-            caption: 'image caption',
+            caption: 'image caption number 1',
             alt: 'description'
         },
         {
             image: image2,
-            caption: 'image caption',
+            caption: 'image caption number 2',
             alt: 'description'
         },
         {
             image: image3,
-            caption: 'image caption',
+            caption: 'image caption number 3',
             alt: 'description'
         },
         {
             image: image4,
-            caption: 'image caption',
+            caption: 'image caption number 4',
             alt: 'description'
         }
     ]
 
+    const totalImages = Object.keys(imageSliderData).length;
+    console.log(totalImages)
+
+    const nextSlide = () => {
+        setCurrentSlide( currentSlide === totalImages -1 ? 0 : currentSlide + 1)
+    }
+    
+
+    const previousSlide = () => {
+        setCurrentSlide( currentSlide === 0 ? totalImages -1 : currentSlide -1)
+    }
+    // console.log(currentSlide)
+
 
     return(
         <div className="slider-gallery-container">
-
-            <h1>IMAGES</h1>
-            {ImageSliderData.map(( slide, index )=> {
+            <i className="fas fa-chevron-circle-left" 
+            onClick={ previousSlide }
+            ></i>
+            <i className="fas fa-chevron-circle-right" 
+            onClick={ nextSlide }
+            ></i>
+            <div className="slider">
+            {imageSliderData.map(( slide, index )=> {
                 console.log(slide.image)
                 
                 return(
-                    
-                    <img className="slider-image" src={slide.image} />
-                    // <img className="slider-image" src={require('../assets/' + slide.image + '.png')} alt={ slide.alt} key={index} />  
+                    <div className={ index === currentSlide ? 'slide active' : 'slide' } key={index}>
+                    {index === currentSlide && (
+                    <>
+                    <img className="slider-image" src={slide.image} alt={ slide.alt} key={index} />
+                    <p className="slider-image-caption">{slide.caption}</p>
+                    </>
+                    )}
+                    </div>
              
                 )
                 
             })
             
             }
-            ;
+            </div>
+            
         </div>
     );
 }
